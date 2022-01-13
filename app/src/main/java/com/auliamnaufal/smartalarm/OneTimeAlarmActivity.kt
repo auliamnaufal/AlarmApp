078275b2232ea22c928b9768a365b69e1c1a6e7f
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2022-01-13 14:20:57 +0700

    finished activity_main.xml and activity_one_time_alarm.xml

---
+++ app/src/main/java/com/auliamnaufal/smartalarm/OneTimeAlarmActivity.kt
@@ -0,0 +1,11 @@
+package com.auliamnaufal.smartalarm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OneTimeAlarmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_one_time_alarm)
+    }
+}

cc622bf2b86cd07d80874eac40725f58a9b5bd0b
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2022-01-17 14:07:59 +0700

    implemented calendar to time and date for activity_main.xml

--- app/src/main/java/com/auliamnaufal/smartalarm/MainActivity.kt
+++ app/src/main/java/com/auliamnaufal/smartalarm/MainActivity.kt
@@ -2,10 +2,39 @@ package com.auliamnaufal.smartalarm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.auliamnaufal.smartalarm.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        initTimeToday()
+        initDateToday()
     }
+
+    private fun initDateToday() {
+        val calendar = Calendar.getInstance()
+        val dateFormat = SimpleDateFormat("EEEE, d MMM yyyy", Locale.getDefault())
+        val formattedDate = dateFormat.format(calendar.time)
+        binding.tvDateToday.text = formattedDate
+    }
+
+    private fun initTimeToday() {
+        // calendar untuk mendapatkan segala hal yang berhubungan dengan waktu di android
+        val calendar = Calendar.getInstance()
+
+        // menentukan format jam yang akan digunakan, e.g. 13.36 atau 01.36 pm
+        val timeFormat = SimpleDateFormat("hh:mm aa", Locale.getDefault())
+        val formattedTime = timeFormat.format(calendar.time)
+        binding.tvTimeToday.text = formattedTime
+    }
+
+
 }
---
+++ app/src/main/java/com/auliamnaufal/smartalarm/RepeatingAlarmActivity.kt
@@ -0,0 +1,11 @@
+package com.auliamnaufal.smartalarm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RepeatingAlarmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repeating_alarm)
+    }
+}
